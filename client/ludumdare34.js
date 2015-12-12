4f7214cf25bedd0e37e434532a3d05bc8dded70b
Rahul Choudhury <rchoudhury@gmail.com>
2015-12-12 21:41:46 +0100

    Updates

--- client/ludumdare34.js
+++ client/ludumdare34.js
@@ -1,13 +1,9 @@
 /*jshint esnext: true */
 
-let Game = {
+Game = {
 
   init() {
-    this.game = new Phaser.Game(640, 480, Phaser.AUTO, '', {
-      preload: Game.preload,
-      create: Game.create,
-      update: Game.update
-    });
+    this.game = new Phaser.Game(640, 480, Phaser.AUTO, '');
     this.game.state.add("Boot", Game.Boot);
     this.game.state.add("Preload", Game.Preload);
     this.game.state.add("Game", Game.Game);
@@ -54,6 +50,17 @@ Game.Preload.prototype = {
 
 Game.Game = function(){}
 Game.Game.prototype = {
+  findObjectsByType(type, map, layer) {
+    var result = [];
+    map.objects[layer].forEach((element) => {
+      if (element.type === type) {
+        element.y -= map.tileHeight;
+        result.push(element);
+      }
+    });
+    return result;
+  },
+
   create() {
     this.map = this.game.add.tilemap("level1");
     this.map.addTilesetImage("tiles", "gameTiles");
@@ -61,6 +68,37 @@ Game.Game.prototype = {
     this.blockedLayer = this.map.createLayer("blockedLayer");
     this.map.setCollisionBetween(1, 2000, true, "blockedLayer");
     this.backgroundLayer.resizeWorld();
+
+    const result = this.findObjectsByType("playerStart", this.map, "objectsLayer");
+    this.player = this.game.add.sprite(result[0].x, result[0].y, "player");
+    this.game.physics.arcade.enable(this.player);
+    this.game.camera.follow(this.player);
+
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+  },
+
+  update() {
+    this.player.body.velocity.x = 0;
+    this.player.body.velocity.y = 0;
+
+    if (this.cursors.up.isDown) {
+      this.player.body.velocity.y -= 64;
+    }
+    else if (this.cursors.down.isDown) {
+      this.player.body.velocity.y += 64;
+    }
+    else if (this.cursors.left.isDown) {
+      this.player.body.velocity.x -= 64;
+    }
+    else if (this.cursors.right.isDown) {
+      this.player.body.velocity.x += 64;
+    }
+
+
+
+    this.game.physics.arcade.collide(this.player, this.blockedLayer);
+    // this.game.physics.arcade.overlap(this.player, this.items, this.collect, null, this);
+    // this.game.physics.arcade.overlap(this.player, this.doors, this.enterDoor, null);
   }
 };
 
